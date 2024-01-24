740bc98abf75b403acc6f0273cfef1987164e891
Teresa Whitesell <tfwhitesell@gmail.com>
2024-01-23 20:18:01 -0600

    more efficient code in app

--- met_mysteries_app/server.R
+++ met_mysteries_app/server.R
@@ -4,17 +4,14 @@
 function(input, output, session) {
 
     output$linePlot <- renderPlot({
-
+        
         weather |> 
             select(year, month_day_constant, tmax_f) |> 
             filter(year %in% input$year) |> 
-            filter(!is.na(tmax_f)) |> 
-            group_by(year, month_day_constant) |> 
-            summarize(high_temp = mean(tmax_f)) |> 
-            ggplot(aes(x = month_day_constant, y = high_temp, color = as.factor(year))) +
+            ggplot(aes(x = month_day_constant, y = tmax_f, color = as.factor(year))) +
             geom_line() +
-            scale_y_continuous(limits = c(min(weather$tmax_f, na.rm = TRUE),
-                                          max(weather$tmax_f, na.rm = TRUE))) +
+            scale_y_continuous(limits = c(min(weather$tmax_f),
+                                          max(weather$tmax_f))) +
             scale_x_date(date_labels = "%B") +
             labs(x = "Month", y = "Daily High Temperature", color = "Year", title = "High Temperature by Day")
 
@@ -25,13 +22,10 @@ function(input, output, session) {
         weather |> 
             select(year, month_day_constant, tmin_f) |> 
             filter(year %in% input$year) |> 
-            filter(!is.na(tmin_f)) |> 
-            group_by(year, month_day_constant) |> 
-            summarize(low_temp = mean(tmin_f)) |> 
-            ggplot(aes(x = month_day_constant, y = low_temp, color = as.factor(year))) +
+            ggplot(aes(x = month_day_constant, y = tmin_f, color = as.factor(year))) +
             geom_line() +
-            scale_y_continuous(limits = c(min(weather$tmin_f, na.rm = TRUE),
-                                          max(weather$tmin_f, na.rm = TRUE))) +
+            scale_y_continuous(limits = c(min(weather$tmin_f),
+                                          max(weather$tmin_f))) +
             scale_x_date(date_labels = "%B") +
             labs(x = "Month", y = "Daily Low Temperature", color = "Year", title = "Low Temperature by Day")
         
