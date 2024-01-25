3080f5a80afe95467dd5ecad7806e5cdb548e854
Teresa Whitesell <tfwhitesell@gmail.com>
2024-01-25 15:29:41 -0600

    changed app ui to use tabsetPanel

--- met_mysteries_app/ui.R
+++ met_mysteries_app/ui.R
@@ -1,25 +1,31 @@
 
 # Define UI for application
+
 fluidPage(
     
-    # Application title
+    # overall title
     titlePanel("Nashville Temperature Analysis 1899-2023"),
     
-    # Sidebar with a multi-select input for years to display
-    multiInput(
-        inputId = "year", label = "Year:",
-        choices = year_choices,
-        selected = min(year_choices)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-        fluidRow(
-            plotOutput("linePlot")
-        ),
-        fluidRow(
-            plotOutput("linePlot2")
-        )
-    ),
+    # page 1
+    tabsetPanel(
+        tabPanel("Year Over Year Comparison",
+                 multiInput(
+                     inputId = "year", label = "Year:",
+                     choices = year_choices,
+                     selected = min(year_choices)
+                 ),
+                 
+                 # Show a plot of the generated distribution
+                 mainPanel(
+                     fluidRow(
+                         plotOutput("linePlot")
+                     ),
+                     fluidRow(
+                         plotOutput("linePlot2")
+                     )
+                 )
+        ) #,
+        # tabPanel("Temperature Difference from Mean"),
+        # tabPanel("another tab")
+    )
 )
-
